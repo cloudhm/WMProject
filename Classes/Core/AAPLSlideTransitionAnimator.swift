ae44da2a6bc772daab9df83b1576452912148b1a
cloud <cloud.huang@whatsmode.com>
2018-08-01 15:08:29 +0800

    Add default tabbar controller

---
+++ Classes/Core/AAPLSlideTransitionAnimator.swift
@@ -0,0 +1,42 @@
+//
+//  AAPLSlideTransitionAnimator.swift
+//  Foo
+//
+//  Created by huangmin on 18/01/2018.
+//  Copyright © 2018 YedaoDev. All rights reserved.
+//
+
+import Foundation
+class AAPLSlideTransitionAnimator : NSObject, UIViewControllerAnimatedTransitioning {
+    var targetEdge : UIRectEdge!
+    init(_ targetEdge : UIRectEdge) {
+        super.init()
+        self.targetEdge = targetEdge
+    }
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.25
+    }
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let fromVC : UIViewController = transitionContext.viewController(forKey: .from) else { return }
+        guard let toVC : UIViewController = transitionContext.viewController(forKey: .to) else { return }
+        let fromView : UIView = fromVC.view
+        let toView : UIView = toVC.view
+        let fromFrame = transitionContext.initialFrame(for: fromVC)
+        // configure vector
+        let offset : CGVector = targetEdge == .left ? CGVector(dx: -1, dy: 0) : CGVector(dx: 1, dy: 0)
+        containerView.addSubview(toView)
+        fromView.frame = fromFrame
+        // reset toView frame
+        toView.frame = fromFrame.offsetBy(dx: fromFrame.size.width * offset.dx * -1,
+                                          dy: fromFrame.size.height * offset.dy * -1)
+        let transitionDuration = self.transitionDuration(using: transitionContext)
+        UIView.animate(withDuration: transitionDuration, animations: {
+            fromView.frame = fromFrame.offsetBy(dx: fromFrame.size.width * offset.dx,
+                                                dy: fromFrame.size.height * offset.dy)
+            toView.frame = fromFrame
+        }) { (finished) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+    }
+}
---
+++ Classes/Core/BaseTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  BaseTabBarController.swift
+//  Foo
+//
+//  Created by huangmin on 16/11/2017.
+//  Copyright © 2017 YedaoDev. All rights reserved.
+//
+
+import UIKit
+public class BaseTabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        for item in tabBar.items! {
+            item.image = item.image?.withRenderingMode(.alwaysOriginal)
+            item.selectedImage = item.selectedImage?.withRenderingMode(.alwaysOriginal)
+            let defaultFont = UIFont.systemFont(ofSize: 9, weight: .regular)
+            item.setTitleTextAttributes([.font : defaultFont, .foregroundColor : #colorLiteral(red: 0.7333333333, green: 0.7333333333, blue: 0.7333333333, alpha: 1) ], for: .normal)
+            item.setTitleTextAttributes([.font : defaultFont, .foregroundColor : #colorLiteral(red: 0.9254901961, green: 0.2392156863, blue: 0.262745098, alpha: 1)], for: .selected)
+            item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -4)
+        }
+        tabBar.tintColor = .white
+        tabBar.itemSpacing = 0
+        tabBar.isTranslucent = false
+        tabBar.backgroundImage = UIImage.imageWithColor(.clear)
+        tabBar.shadowImage = UIImage.imageWithColor(UIColor.colorWithHexString(Constant.color_F3F3F3))
+        delegate = self
+    }
+}
+// MARK: UITabBarControllerDelegate
+extension BaseTabBarController : UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        guard let fromIndex = tabBarController.viewControllers?.index(of: fromVC) else { return nil }
+        guard let toIndex = tabBarController.viewControllers?.index(of: toVC) else { return nil }
+        /**
+         * tabbarController's viewControllers trainsitioning animation
+         */
+        return AAPLSlideTransitionAnimator(fromIndex < toIndex ? .left : .right)
+    }
+}
