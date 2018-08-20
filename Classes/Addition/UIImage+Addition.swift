9a06ae1d8679ec64bf737860dd5bf3d305bd576b
cloud <cloud.huang@whatsmode.com>
2018-08-20 09:39:46 +0800

    update uiimage extension

--- Classes/Addition/UIImage+Addition.swift
+++ Classes/Addition/UIImage+Addition.swift
@@ -30,15 +30,43 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return image!
     }
-    public static func compressImage(_ image : UIImage?, _ compressionQuality : CGFloat? = nil)-> Data? {
-        if let image = image {
-            if let imageData = UIImageJPEGRepresentation(image, compressionQuality != nil ? compressionQuality! : 1) {
-                while (imageData.count > 1024 * 1024 * 3) {
-                    return compressImage(UIImage(data: imageData), 0.9)
-                }
-                return imageData
+    // MARK: compress image, image quantity and image size
+    // https://www.cnblogs.com/silence-cnblogs/p/6346729.html
+    public static func compressImage(_ image: UIImage, toByte maxLength: Int? = 1024 * 1024 * 3) -> Data? {
+        var compression: CGFloat = 1
+        guard var data = UIImageJPEGRepresentation(image, compression), let maxLength = maxLength,
+            data.count > maxLength else { return UIImageJPEGRepresentation(image, compression) }
+        
+        // Compress by size
+        var max: CGFloat = 1
+        var min: CGFloat = 0
+        for _ in 0..<6 {
+            compression = (max + min) / 2
+            data = UIImageJPEGRepresentation(image, compression)!
+            if CGFloat(data.count) < CGFloat(maxLength) * 0.9 {
+                min = compression
+            } else if data.count > maxLength {
+                max = compression
+            } else {
+                break
             }
         }
-        return nil
+        var resultImage: UIImage = UIImage(data: data)!
+        if data.count < maxLength { return data }
+        
+        // Compress by size
+        var lastDataLength: Int = 0
+        while data.count > maxLength, data.count != lastDataLength {
+            lastDataLength = data.count
+            let ratio: CGFloat = CGFloat(maxLength) / CGFloat(data.count)
+            let size: CGSize = CGSize(width: Int(resultImage.size.width * sqrt(ratio)),
+                                      height: Int(resultImage.size.height * sqrt(ratio)))
+            UIGraphicsBeginImageContext(size)
+            resultImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            resultImage = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            data = UIImageJPEGRepresentation(resultImage, compression)!
+        }
+        return data
     }
 }
