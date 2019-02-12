cf94fa025a23c73edd813b49984883a73c4bd5c8
cloud <cloud.huang@whatsmode.com>
2019-02-12 20:01:23 +0800

    fixed

--- Classes/Addition/Others/MDCache.swift
+++ Classes/Addition/Others/MDCache.swift
@@ -13,26 +13,3 @@ public class MDCache {
         SDImageCache.shared().clearMemory()
     }
 }
-
-public class MDImagePrefetcher {
-    private static let ignoreNetwork = true
-    private static let shared = MDImagePrefetcher()
-    private lazy var manager : SDWebImageManager = {
-        let m = SDWebImageManager()
-        m.imageDownloader?.maxConcurrentDownloads = 3
-        return m
-    }()
-    public static func prefetchImageURL(_ imageURL : URL?) {
-        let isReachableOnEthernetOrWiFi = MDNetworkMonitor.shared.isReachableOnEthernetOrWiFi
-        if isReachableOnEthernetOrWiFi || ignoreNetwork {
-            MDImagePrefetcher.shared.manager.loadImage(with: imageURL,
-                                                       options: .lowPriority,
-                                                       progress: nil) { (_, _, _, _, _, _) in}
-        }
-    }
-    public static func prefetchImageURLs(_ imageURLs : [URL]?) {
-        for imageURL in imageURLs ?? [] {
-            prefetchImageURL(imageURL)
-        }
-    }
-}
---
+++ Classes/Core/Network/MDImagePrefetcher.swift
@@ -0,0 +1,32 @@
+//
+//  MDImagePrefetcher.swift
+//  WMProject
+//
+//  Created by cloud on 2019/2/12.
+//  Copyright © 2019 Yedao Inc. All rights reserved.
+//
+
+import Foundation
+import SDWebImage
+public class MDImagePrefetcher {
+    private static let ignoreNetwork = true
+    private static let shared = MDImagePrefetcher()
+    private lazy var manager : SDWebImageManager = {
+        let m = SDWebImageManager()
+        m.imageDownloader?.maxConcurrentDownloads = 3
+        return m
+    }()
+    public static func prefetchImageURL(_ imageURL : URL?) {
+        let isReachableOnEthernetOrWiFi = MDNetworkMonitor.shared.isReachableOnEthernetOrWiFi
+        if isReachableOnEthernetOrWiFi || ignoreNetwork {
+            MDImagePrefetcher.shared.manager.loadImage(with: imageURL,
+                                                       options: .lowPriority,
+                                                       progress: nil) { (_, _, _, _, _, _) in}
+        }
+    }
+    public static func prefetchImageURLs(_ imageURLs : [URL]?) {
+        for imageURL in imageURLs ?? [] {
+            prefetchImageURL(imageURL)
+        }
+    }
+}
